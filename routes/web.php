14cc3f2df8aa2ab1d4eff227ccd1579579c59d77
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2024-06-01 17:51:30 +0530

    add new route to test git actions

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('comingsoon');
 });
+
+Route::get('/gitwrk', function () {
+    return "current changes build using git actions";
+});

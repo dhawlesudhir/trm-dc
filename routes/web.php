670f77f9f9baeb58deac9ab989b0963898e9cb7e
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2024-06-01 22:38:30 +0530

    removed test route and replaced with build workflow

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('comingsoon');
 });
-
-Route::get('/gitwrk', function () {
-    return "current changes build using git actions";
-});

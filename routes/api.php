b3556ef5553c953be5b1aa3a5e5988ff4cd8d3b9
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2024-06-15 11:02:29 +0530

    add get users feature

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::post('/login', [UserController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/signup', [UserController::class, 'signup']);
-Route::post('/update-password', [UserController::class, 'updatePassword']);
+Route::post('/update-password', [AuthController::class, 'updatePassword']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/users', 'index');
+    });
 });

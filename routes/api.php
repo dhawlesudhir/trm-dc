877401515e315ad1ce2bfeddc6cb485f55432b54
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2024-06-15 17:33:22 +0530

    add names and default route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('api.login');
 Route::post('/signup', [UserController::class, 'signup']);
 Route::post('/update-password', [AuthController::class, 'updatePassword']);
 
@@ -12,4 +12,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::controller(UserController::class)->group(function () {
         Route::get('/users', 'index');
     });
+});
+
+Route::fallback(function () {
+    return "Not a valid api request";
 });
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('comingsoon');
 });
+
+Route::get('/login', function () {
+    return "please login first";
+})->name('login');
+
+Route::fallback(function () {
+    return "Not a valid request";
+});

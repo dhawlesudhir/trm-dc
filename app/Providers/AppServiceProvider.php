54a2c20b8bd36d510a0ef54c5852ca1675687cf3
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2024-07-02 20:47:08 +0530

    add gates for authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+
+        Gate::before(function ($user, $ability) {
+            if ($user->user_type == "A") {
+                return true;
+            }
+        });
+
+        Gate::define('get-all-users', function () {
+            return false;
+        });
+
+        Gate::define('user-management', function () {
+            return false;
+        });
+
+        Gate::define('get-all-retailers', function (User $user) {
+            return in_array($user->user_type, ['A', 'D', 'R']);
+        });
+
+        Gate::define('update-profile', function (User $user, User $profile) {
+            return $user->login_id == $profile->login_id;
+        });
+
+        // Gate::define('get-all-retailers', [UserController::class, 'retailers']);
     }
 }

9c7efc0b9bb2c74a9f6db979b389dae9076bf518
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2024-07-02 20:43:14 +0530

    add helper function to detect is user admin

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -2,6 +2,8 @@
 
 namespace App\Helpers;
 
+use Illuminate\Support\Facades\Auth;
+
 class Helper
 {
     public static function password_generate(): string
@@ -10,5 +12,15 @@ class Helper
         $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
         // Shuffle the characters in the string and extract a substring of length $chars
         return substr(str_shuffle($data), 0, 8);
+    }
+
+    public static function user_is_admin(): bool
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            return $user->user_type == 'A';
+        } else {
+            return false;
+        }
     }
 }

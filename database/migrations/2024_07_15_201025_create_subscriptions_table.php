054c374908d783ad5c5e664bb5f2b5eb8be35d63
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2024-07-15 21:36:16 +0530

    add subscriptions table

---
+++ database/migrations/2024_07_15_201025_create_subscriptions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->comment("commission plans");
+            $table->uuid('id');
+            $table->string("plan_name")->default('base')->comment("base, medium, super, prime");
+            $table->string("service_name")->comment("service->service_name");
+            $table->string("operator/bank");
+            $table->decimal("commission", 8, 2)->default(00.0)->nullable(false);
+            $table->set("com_type", ['%', 'fixed'])->default('%')->nullable(false);
+            $table->softDeletes("deleted_at");
+            $table->timestamps();
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};

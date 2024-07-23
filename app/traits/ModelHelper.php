582c108de09a23b267086c966a2e993caaf455d1
dhawlesudhir <34824916+dhawlesudhir@users.noreply.github.com>
2024-07-23 13:42:32 +0530

    ModelHelper updated

--- app/traits/ModelHelper.php
+++ app/traits/ModelHelper.php
@@ -2,32 +2,35 @@
 
 namespace App\traits;
 
+use App\Models\DmtBeneficiary;
+use App\Models\DmtCustomer;
 use App\Models\Service;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 
 trait ModelHelper
 {
 
-    public function TransactionNumber(String $prefix = null)
+    public function uniqid()
     {
         $id = $this->id;
         $postfixLength = strlen($id) + 2;
+        $prefix = $this->getPrefix();
         if ($prefix) {
             return $prefix . str_pad($id, $postfixLength, '0', STR_PAD_LEFT);
         } else {
             $prefix = Service::find($this->service_id)->service_code ?? 'NA';
             return  $prefix . str_pad($id, $postfixLength, '0', STR_PAD_LEFT);
         }
+    }
 
-        // return Attribute::make(
-        //     get: function () use ($prefix) {
-        //         if ($prefix != '') {
-        //             return $prefix . str_pad($this->id, 4, '0', STR_PAD_LEFT);
-        //         } else {
-        //             $prefix = Service::find($this->service_id)->service_code ?? 'NA';
-        //             return  $prefix . str_pad($this->id, strlen($this->id) + 2, '0', STR_PAD_LEFT);
-        //         }
-        //     }
-        // );
+    public function getPrefix()
+    {
+        return match (get_class($this)) {
+            'App\Models\DmtBeneficiary' => 'CUST',
+            'App\Models\DmtCustomer' => 'BNFC',
+            'App\Models\Ledger' => 'TRN',
+            'App\Models\Service' => 'SERVE',
+            default => null
+        };
     }
 }
